7c41442738d826b7dc978b4e9a14b7003ea083bb
Richi Rodriguez Gonzalez <ricardorg20@gmail.com>
2020-05-08 23:10:19 +0200

    First Commit

---
+++ falken_energy.py
@@ -0,0 +1,7 @@
+# by Richi Rod AKA @richionline / falken20
+
+
+import requests
+
+if __name__ == '__main__':
+    pass
---
+++ falken_temp.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This previous line is useless inPython3
+
+"""
+API Daikin: https://github.com/ael-code/daikin-control
+"""
+
+import requests
+from time import sleep
+
+AIR_CONDITIONERS = {u'Salón': 'ip.ip.ip.ip', 'Other Room': 'ip.ip.ip.ip'}
+
+def read_temp():
+    """ Calls sensor information """
+    for machine in AIR_CONDITIONERS:
+        print("ROD: Establish connection with %s and url http://%s/aircon/get_sensor_info" % (machine, AIR_CONDITIONERS[machine]))
+        r = requests.get(url="http://%s/aircon/get_sensor_info" % AIR_CONDITIONERS[machine])
+        response = r.text
+        parameters = response.split(',')
+        for parameter in parameters:
+            if parameter.startswith("htemp"):
+                inside = float(parameter.split("=")[1])
+            if parameter.startswith("otemp"):
+                outside = float(parameter.split("=")[1])
+        print (u"%s: %dºC %dºC" % (machine, inside, outside))
+
+
+# Detail doc of "__name__ == __main__" use: https://es.stackoverflow.com/questions/32165/qu%C3%A9-es-if-name-main
+if __name__ == '__main__':
+    while True:
+        read_temp()
+        sleep(60)
