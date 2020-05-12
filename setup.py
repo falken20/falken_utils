2e1d49d6d1cd5f591e641e7db1d78639a3ee43cc
Richi Rodriguez Gonzalez <ricardorg20@gmail.com>
2020-05-12 11:28:54 +0200

    First Commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+from src import __version__
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def get_requirements():
+    """
+    Returns a list of dependencies from the `requirements.txt` file
+    """
+    dep = []
+
+    with open("requirements.txt", "r") as fp:
+        line = fp.readline()
+        while line:
+            dep.append(line.strip())
+            line = fp.readline()
+
+    return dep
+
+
+setuptools.setup(
+    name="falken_home",
+    version=__version__,
+    author="Richi Rod",
+    author_email="ricardorg20@gmail.com",
+    description="Get any data from Home consuming",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/falken20/falken_home",
+    install_requires=get_requirements(),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+# by Richi Rod AKA @richionline / falken20
+
+__version__ = '0.0.1'
