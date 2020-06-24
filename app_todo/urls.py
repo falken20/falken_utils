fb1384a39723020a8c4788384c308267f9831b7e
Richi Rodriguez Gonzalez <>
2020-06-24 16:03:46 +0200

    Add english app structure

--- app_todo/urls.py
+++ app_todo/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.todo_view, name='todo_view'),
     path('add_todo/', views.add_todo, name='add_todo'),
     path('delete_todo/<int:todo_id>/', views.delete_todo, name='delete_todo'),
-}
+]
