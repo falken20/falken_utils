f60c4cf37723d182f9134261f141b05c72185611
Richi Rodriguez Gonzalez <>
2020-06-26 23:51:13 +0200

    Word card and styles

--- app_english_dic/models.py
+++ app_english_dic/models.py
@@ -3,19 +3,19 @@ from django.db import models
 
 class WordTypeItem(models.Model):
     """ Class for the different types of words: name, adjective, adverb, etc."""
-    word_type = models.TextField();
+    word_type = models.TextField()
 
     def __str__(self):
         return self.word_type
 
 
-class EnglishItem(models.Model):
+class WordItem(models.Model):
     """ Class for word in english and spanish"""
     word_en = models.TextField()
     word_es = models.TextField()
     word_type = models.ForeignKey(WordTypeItem, on_delete=models.PROTECT)
-    word_times = models.IntegerField(default=0)  # Times that this word was shown
+    word_times = models.IntegerField(default=0)  # Times this word was shown
 
     def __str__(self):
-        return self.word_en + ' - ' + self.word_es + ' ----> ' + self.word_type
+        return self.word_en + ' - ' + self.word_es
 
