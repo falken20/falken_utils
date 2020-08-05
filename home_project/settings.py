4d1279187efd38b95207c9a24f3eab379752b72d
Richi Rodriguez Gonzalez <ricardorg20@gmail.com>
2020-08-05 11:18:52 +0200

    Preparing PostgreSQL DB for production environment

--- home_project/settings.py
+++ home_project/settings.py
@@ -147,3 +147,20 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+# About Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
