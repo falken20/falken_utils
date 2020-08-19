6f9668060523df1a063b3c718b1b3eb0d760fda4
Richi <ricardorg20@gmail.com>
2020-08-19 08:31:30 +0200

    Created screen for books form

--- app_books/urls.py
+++ app_books/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.books_view, name='books_view'),
+    path('add_book/', views.books_form, name='add_book'),
 ]
--- app_books/views.py
+++ app_books/views.py
@@ -7,6 +7,7 @@ from.models import BookItem, AuthorItem
 
 
 def books_view(request):
+    """ Show the screen with all the books order by year """
 
     logging.info(f'{os.getenv("ID_LOG", "")} Getting all the books in the DB')
 
@@ -14,3 +15,14 @@ def books_view(request):
     template_name = 'books/books.html'
 
     return render(request, template_name, {'books': queryset})
+
+
+def books_form(request):
+    """ Show the form for adding books """
+
+    logging.info(f'{os.getenv("ID_LOG", "")} Showing the books form')
+
+    queryset = AuthorItem.objects.all().order_by('author_surname', 'author_name')
+    template_name = 'books/books_form.html'
+
+    return render(request, template_name, {'authors': queryset})
