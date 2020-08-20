fbc4a00759f79c7ffb6a6af51fe7dc83ec73ce33
Richi <ricardorg20@gmail.com>
2020-08-20 14:29:01 +0200

    Books and authors admin forms

--- app_books/urls.py
+++ app_books/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new_book/', views.books_form, name='new_book'),
-    path('add_book/', views.add_book, name='add_book'),
+    path('', views.books_view, name='books_view'),
+    path('new_book/', views.books_form, name='form_new_book'),
+    path('add_book/', views.add_book, name='create_new_book'),
+
+    path('new_author/', views.authors_form, name='form_new_author'),
+    path('add_author/', views.add_author, name='create_new_author'),
 ]
