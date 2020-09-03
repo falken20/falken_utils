5da0b613e245a5883d4e58ee0cbcc8238cf835e1
Richi <ricardorg20@gmail.com>
2020-09-03 10:41:06 +0200

    Fix bugs for weather cron Heroku

--- home_project/__init__.py
+++ home_project/__init__.py
@@ -3,7 +3,9 @@
 __title__ = 'Falken Home'
 __version__ = '1.1.0'
 __author__ = 'Richi Rod'
-__url__ = 'https://github.com/falken20/'
+__url_github__ = 'https://github.com/falken20/'
+__url_twitter__ = 'https://twitter.com/richionline'
+__url_linkedin__ = 'https://www.linkedin.com/in/richionline/'
 __license__ = 'MIT License'
 __copyright__ = '© 2020 by Richi Rod AKA @richionline / falken20'
 __features__ = [
@@ -22,7 +24,9 @@ SETUP_DATA = {
     'title': __title__,
     'version': __version__,
     'author': __author__,
-    'url': __url__,
+    'url_github': __url_github__,
+    'url_twitter': __url_twitter__,
+    'url_linkedin': __url_linkedin__,
     'license': __license__,
     'copyrigth': __copyright__,
     'features': __features__,
